8be6a58b02a3bae084f29608333d926c70684951
lovelylif3 <136279746+lovelylif3@users.noreply.github.com>
2024-03-01 23:22:11 -0600

    Add conditions for win, lose and tie

--- rps.js
+++ rps.js
@@ -2,15 +2,43 @@ console.log("Welcome! Please choose rock, paper or scissors in the prompt. When 
 
 let playerChoice = prompt("Rock, paper, or scissors?").toLowerCase;
 
-function getComputerChoice() {
-    const choiceOptions = ["Rock", "Paper", "Scissors"];
-    let computerChoice = choiceOptions[Math.floor(Math.random() * choiceOptions.length)];
-    return computerChoice
 
-}
+ function playRound(computerChoice, playerChoice) {
 
-// function playRound(computerChoice, playerChoice) {
-//     switch 
-//     // (playerChoice === "rock" && computerChoice === "scissors")
+    function getComputerChoice() {
+        const choiceOptions = ["Rock", "Paper", "Scissors"];
+        let computerChoice = choiceOptions[Math.floor(Math.random() * choiceOptions.length)];
+        return computerChoice
+    }
 
-// }
+         if ((playerChoice === 'rock' && computerChoice === 'scissors') || (playerChoice === 'scissors' && 'computerChoice' === 'paper') || (playerChoice === 'paper' && computerChoice === 'rock')) {
+            return "You win!" + playerChoice + " beats " + computerChoice + "!";
+        } else if (playerChoice === 'scissors' && 'computerChoice' === 'paper'); {
+            return "You win! scissors beat paper";
+        // } else if (playerChoice === 'paper' && computerChoice === 'rock'); {
+        //     return "You win! paper beats rock";
+        // } else if (playerChoice === 'rock' && computerChoice === 'paper'); {
+        //     return "You lose! paper beats rock"
+        // } else if (playerChoice === 'paper' && computerChoice === 'scissors'); {
+        //     return "You lose! scissors beats paper"
+        // } else if (playerChoice === 'scissors' && computerChoice === 'rock'); {
+        //     return "You lose! rock beats scissors"
+        // } else if (playerChoice === 'paper' && computerChoice === 'paper'); {
+        //     return "Tie! rock vs rock"
+        // } else if (playerChoice === 'paper' && computerChoice === 'rock'); {
+        //     return "Tie! paper vs paper"
+        // } else if (playerChoice === 'paper' && computerChoice === 'rock'); {
+        //     return "Tie! scissors vs scissors"
+        }
+
+ }
+
+//
+// playerChoice === scissors && computerChoice === paper
+// playerChoice === paper && computerChoice === rock
+// playerChoice === rock && computerChoice === scissors
+// playerChoice === rock && computerChoice === scissors
+// playerChoice === rock && computerChoice === scissors
+// playerChoice === rock && computerChoice === scissors
+    
+// // }
