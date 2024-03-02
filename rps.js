3963b81b6037e5bec280ab8a6196e2b2568954f5
lovelylif3 <136279746+lovelylif3@users.noreply.github.com>
2024-03-01 22:11:21 -0600

    Add console message and function for first round

--- rps.js
+++ rps.js
@@ -1,6 +1,15 @@
+console.log("Welcome! Please set playerChoice to rock, paper or scissors. When you're ready, execute the playRound function.")
+
+prompt("Rock, paper, or scissors?").toLowerCase;
+
 function getComputerChoice() {
     const choiceOptions = ["Rock", "Paper", "Scissors"];
-    let choice = choiceOptions[Math.floor(Math.random() * choiceOptions.length)];
-    return choice
+    let computerChoice = choiceOptions[Math.floor(Math.random() * choiceOptions.length)];
+    return computerChoice
 
+}
+
+function playRound(computerChoice, playerChoice) {
+    if (playerChoice === "rock" && computerChoice === "scissors")
+        
 }
