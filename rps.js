99608db8fdc2928b8145b6dd932710dbf196f85e
lovelylif3 <136279746+lovelylif3@users.noreply.github.com>
2024-03-02 12:20:24 -0600

    Make computerChoice random when playRound runs

--- rps.js
+++ rps.js
@@ -1,13 +1,7 @@
-console.log("Welcome! Please set playerChoice to rock, paper or scissors. When you're ready to play, execute the playRound function.")
+console.log("Welcome! Please set playerChoice to rock, paper or scissors. When you're ready to play, execute playRound(computerChoice, playerChoice) function.")
 
 let playerChoice = prompt("Rock, paper, or scissors?")
-let computerChoice = getComputerChoice()
-
-
-// function getPlayerChoice() {
-//     let result = prompt("Rock, paper, or scissors?").toLowerCase
-//     return result   
-// }
+let computerChoice
 
 function getComputerChoice() {
     const choiceOptions = ["Rock", "Paper", "Scissors"];
@@ -16,25 +10,17 @@ function getComputerChoice() {
 }
 
  function playRound(computerChoice, playerChoice) {
-    
-         if ((playerChoice === 'rock' && computerChoice === 'scissors') || (playerChoice === 'scissors' && 'computerChoice' === 'paper') || (playerChoice === 'paper' && computerChoice === 'rock')) {
-            return "You win! " + playerChoice + " beats " + computerChoice + "!";
-        } else if ((playerChoice === 'rock' && computerChoice === 'paper') || (playerChoice === 'paper' && computerChoice === 'scissors') || (playerChoice === 'scissors' && computerChoice === 'rock')) {
-            return "You lose! " + computerChoice + " beats " + playerChoice + "!";
-        } else if ((playerChoice === 'paper' && computerChoice === 'paper') ||(playerChoice === 'paper' && computerChoice === 'rock') || (playerChoice === 'paper' && computerChoice === 'rock')) {
-            return "Tie! You both picked " + computerChoice + ".";
-        } else {
-            return "Something went wrong."
-        }
+
+    computerChoice = getComputerChoice()
+
+    if ((playerChoice === 'rock' && computerChoice === 'scissors') || (playerChoice === 'scissors' && computerChoice === 'paper') || (playerChoice === 'paper' && computerChoice === 'rock')) {
+        return "You win! " + playerChoice + " beats " + computerChoice + "!";
+    } else if ((playerChoice === 'rock' && computerChoice === 'paper') || (playerChoice === 'paper' && computerChoice === 'scissors') || (playerChoice === 'scissors' && computerChoice === 'rock')) {
+        return "You lose! " + computerChoice + " beats " + playerChoice + "!";
+    } else if ((playerChoice === 'paper' && computerChoice === 'paper') ||(playerChoice === 'rock' && computerChoice === 'rock') || (playerChoice === 'scissors' && computerChoice === 'scissors')) {
+        return "Tie! You both picked " + computerChoice + ".";
+    } else {
+        return "Something went wrong."
+    }
 
  }
-
-//
-// playerChoice === scissors && computerChoice === paper
-// playerChoice === paper && computerChoice === rock
-// playerChoice === rock && computerChoice === scissors
-// playerChoice === rock && computerChoice === scissors
-// playerChoice === rock && computerChoice === scissors
-// playerChoice === rock && computerChoice === scissors
-    
-// // }
